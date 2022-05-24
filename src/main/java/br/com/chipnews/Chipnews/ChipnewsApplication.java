f25d468e28357bc5277f08a2f22cf973bb3d78dd
Guilherme Bastos <guibastos776@gmail.com>
2022-05-24 15:53:23 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/chipnews/Chipnews/ChipnewsApplication.java
@@ -0,0 +1,13 @@
+package br.com.chipnews.Chipnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chipnews/Chipnews/ChipnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chipnews.Chipnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
