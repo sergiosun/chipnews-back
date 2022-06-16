01cf47b621e830463264e356d7a1c8a53703b725
Guilherme Bastos <guibastos776@gmail.com>
2022-06-16 13:10:13 -0300

    Autenticação

--- src/main/java/br/com/chipnews/config/SecurityConfig.java
+++ src/main/java/br/com/chipnews/config/SecurityConfig.java
@@ -1,53 +1,40 @@
 package br.com.chipnews.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 
-  /*  @Autowired
-    private Environment env;
-
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable();
+    protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .anyRequest().authenticated();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-*//*                .and()
-                .httpBasic();*//*
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }*/
-
-
-    /*@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", passwordEncoder.encode("test"));
         auth.inMemoryAuthentication()
-                .withUser("chipnews").password("123456").roles("USER")
+                .withUser("chipnews")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER , ADMIN")
                 .and()
-                .withUser("admin").password("654321").roles("USER","ADMIN");
-    }*/
-
+                .withUser("usuario")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
 
 }
